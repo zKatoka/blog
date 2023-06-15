64a75b840798739329b5c718f5bd99873e49b0dd
zkatoka <emailderecuperacao098@gmail.com>
2023-06-15 15:17:18 -0300

    botão verde

--- app/Views/header.php
+++ app/Views/header.php
@@ -16,8 +16,8 @@
                     </li>
                 </ul>
                                 <span class="navbar-text">
-                    <a class="btn btn-info" href="#" data-tooltip="tooltip" title="Não tem uma conta? Cadastre-se">Cadastre-se</a>
-                    <a class="btn btn-info" href="#" data-tooltip="tooltip" title="Tem uma conta? Faça login">Entrar</a>
+                    <a class="btn btn-success" href="#" data-tooltip="tooltip" title="Não tem uma conta? Cadastre-se">Cadastre-se</a>
+                    <a class="btn btn-success" href="#" data-tooltip="tooltip" title="Tem uma conta? Faça login">Entrar</a>
                 </span>
                             </div>
         </nav>
